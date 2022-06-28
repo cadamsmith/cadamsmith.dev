d7b55107b07fad108f19842c09322c7910fc4522
cadamsmith <cadamsmith.dev@gmail.com>
2022-06-28 00:49:37 -0500

    remove creator and last updater from base entity class

---
+++ PortfolioAPI/Migrations/20220628052049_firstMigration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PortfolioAPI.Migrations
+{
+    public partial class firstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blurbs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    LastModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "text", nullable: true),
+                    IsArchived = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blurbs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blurbs");
+        }
+    }
+}
--- PortfolioAPI/Models/Common/BaseEntity.cs
+++ PortfolioAPI/Models/Common/BaseEntity.cs
@@ -11,9 +11,7 @@ public abstract class BaseEntity
     public int Id { get; set; }
 
     public DateTime CreatedAt { get; set; }
-    public string CreatedBy { get; set; }
     public DateTime LastModifiedAt { get; set; }
-    public string LastModifiedBy { get; set; }
 
     public bool IsArchived { get; set; }
 }
