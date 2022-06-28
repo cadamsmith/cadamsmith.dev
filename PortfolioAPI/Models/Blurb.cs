72bcc19e5d83a5b9bc9868dc6e0b5472c87c1a39
cadamsmith <cadamsmith.dev@gmail.com>
2022-06-28 00:52:54 -0500

    add constructors to entity models

--- PortfolioAPI/Models/Blurb.cs
+++ PortfolioAPI/Models/Blurb.cs
@@ -4,6 +4,12 @@ namespace PortfolioAPI.Models;
 
 public class Blurb : BaseEntity
 {
+    public Blurb(string name, string content) : base()
+    {
+        Name = name;
+        Content = content;
+    }
+
     public string Name { get; set; }
     public string Content { get; set; }
 }
--- PortfolioAPI/Models/Common/BaseEntity.cs
+++ PortfolioAPI/Models/Common/BaseEntity.cs
@@ -6,6 +6,12 @@ namespace PortfolioAPI.Models.Common;
 
 public abstract class BaseEntity
 {
+    public BaseEntity()
+    {
+        CreatedAt = LastModifiedAt = DateTime.Now;
+        IsArchived = false;
+    }
+
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Key]
     public int Id { get; set; }
