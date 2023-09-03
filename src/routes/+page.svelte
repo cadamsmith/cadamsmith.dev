0de286ca44dee5ffe4a1b0f638634ea48b15be9c
cadamsmith <cadamsmith.dev@gmail.com>
2023-09-03 10:08:52 -0500

    made grid system for main element

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
-<section class="hero">
+<section class="hero full-width">
 	<h1>Hi, I'm Adam!</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</section>
+
+<section>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </section>
 
