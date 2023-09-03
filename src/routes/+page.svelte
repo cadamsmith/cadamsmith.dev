4e998132783c9e24aef74ca6571b6479782874db
cadamsmith <cadamsmith.dev@gmail.com>
2023-09-03 12:07:45 -0500

    verbiage on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,21 @@
 <section class="hero full-width">
-	<h1>Hi, I'm Adam!</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<h1>Hey, I'm Adam!</h1>
+	<p>
+		I'm a software engineer. I'm currently working as a technical lead for
+		<a href="https://cws.auburn.edu/cws" target="_blank">Campus Web Solutions</a> at Auburn University.
+	</p>
+
+	<p>
+		My technology area at work is ASP.NET full-stack web development, but most of my job is
+		mentoring developers and helping them learn.
+	</p>
+
+	<p>
+		I also do a lot of internal tooling and automation. I'm a big fan of GitHub Actions and Azure
+		DevOps.
+	</p>
+
+	<a href="/resume">Resume</a>
 </section>
 
 <section>
