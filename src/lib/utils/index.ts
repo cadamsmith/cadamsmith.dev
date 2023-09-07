a842e5ec2b9947c73b1b769769a060b59c578a7d
cadamsmith <cadamsmith.dev@gmail.com>
2023-09-07 14:14:33 -0500

    move posts out to their own folder

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,17 +2,17 @@ import type { Post } from '$lib/types/Post';
 import type { PostMetadata } from '$lib/types/PostMetadata';
 
 export const fetchMarkdownPosts = async (): Promise<Post[]> => {
-	const allPostFiles = import.meta.glob('/src/routes/blog/*.md');
+	const allPostFiles = import.meta.glob('/posts/*.md');
 	const iterablePostFiles = Object.entries(allPostFiles);
 
 	const allPosts = await Promise.all(
 		iterablePostFiles.map(async ([path, resolver]) => {
 			const { metadata } = (await resolver()) as { metadata: PostMetadata };
-			const postPath = path.slice(11, -3);
+			const slug = (path.split('/').pop() ?? '').split('.')[0];
 
 			return {
 				meta: metadata,
-				path: postPath
+				path: `/blog/${slug}`
 			};
 		})
 	);
