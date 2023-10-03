8c1fabbba702e7eed7a106d2b721c74013f457ce
cadamsmith <cadamsmith.dev@gmail.com>
2023-10-03 00:06:05 -0500

    under construction reusable system

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,32 +1,9 @@
-<script lang="ts">
-	import { dev } from '$app/environment';
-
-	const inConstruction = !dev;
-</script>
-
 <svelte:head>
 	<title>Portfolio - Adam Smith</title>
 </svelte:head>
 
-{#if inConstruction}
-	<section class="construction">
-		<h1>Under Construction</h1>
+<section class="hero">
+	<h1>Portfolio</h1>
 
-		<iconify-icon class="big-icon" icon="mdi:account-hard-hat" />
-	</section>
-{:else}
-	<section class="hero">
-		<h1>Portfolio</h1>
-
-		<h2>test test test</h2>
-	</section>
-{/if}
-
-<style>
-	iconify-icon {
-		width: 100%;
-		display: flex;
-		justify-content: center;
-		color: #ff9e0d;
-	}
-</style>
+	<h2>test test test</h2>
+</section>
